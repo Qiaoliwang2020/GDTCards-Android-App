3e4bc0ff29bbd92a3d82bab11c05fa175a2812e2
Qiaoliwang2020 <414217795zoe@gmail.com>
2021-05-02 13:58:30 +1000

    generate bar code according to card id

--- app/src/main/java/com/example/dgtcards/cardDetailsActivity.kt
+++ app/src/main/java/com/example/dgtcards/cardDetailsActivity.kt
@@ -1,9 +1,14 @@
 package com.example.dgtcards
 
+import android.graphics.Bitmap
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.ColorInt
+import androidx.core.content.ContextCompat
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.oned.Code128Writer
 import kotlinx.android.synthetic.main.activity_card_details.*
-import kotlinx.android.synthetic.main.activity_transaction_details.*
 import kotlinx.android.synthetic.main.card.*
 
 class cardDetailsActivity : AppCompatActivity() {
@@ -24,5 +29,63 @@ class cardDetailsActivity : AppCompatActivity() {
         created_time.text = "Created Time: "+ itemModel!!.createTime
         card_background.setBackgroundColor(itemModel!!.cardBackground)
         page_title.text = itemModel!!.city
+
+
+        displayBitmap(itemModel!!.id)
+    }
+
+    private fun createBarcodeBitmap(
+        barcodeValue: String,
+        @ColorInt barcodeColor: Int,
+        @ColorInt backgroundColor: Int,
+        widthPixels: Int,
+        heightPixels: Int
+    ): Bitmap {
+        val bitMatrix = Code128Writer().encode(
+            barcodeValue,
+            BarcodeFormat.CODE_128,
+            widthPixels,
+            heightPixels
+        )
+
+        val pixels = IntArray(bitMatrix.width * bitMatrix.height)
+        for (y in 0 until bitMatrix.height) {
+            val offset = y * bitMatrix.width
+            for (x in 0 until bitMatrix.width) {
+                pixels[offset + x] =
+                    if (bitMatrix.get(x, y)) barcodeColor else backgroundColor
+            }
+        }
+
+        val bitmap = Bitmap.createBitmap(
+            bitMatrix.width,
+            bitMatrix.height,
+            Bitmap.Config.ARGB_8888
+        )
+        bitmap.setPixels(
+            pixels,
+            0,
+            bitMatrix.width,
+            0,
+            0,
+            bitMatrix.width,
+            bitMatrix.height
+        )
+        return bitmap
+    }
+
+    private fun displayBitmap(value: String) {
+        val widthPixels = resources.getDimensionPixelSize(R.dimen.width_barcode)
+        val heightPixels = resources.getDimensionPixelSize(R.dimen.height_barcode)
+
+        image_barcode.setImageBitmap(
+            createBarcodeBitmap(
+                barcodeValue = value,
+                barcodeColor = ContextCompat.getColor(baseContext,R.color.black),
+                backgroundColor = ContextCompat.getColor(baseContext,android.R.color.white),
+                widthPixels = widthPixels,
+                heightPixels = heightPixels
+            )
+        )
     }
 }
