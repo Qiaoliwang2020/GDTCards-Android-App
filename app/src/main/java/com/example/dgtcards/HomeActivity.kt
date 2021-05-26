55fa3d18eeb06885e1ebf16b30472d6f36984c0a
Qiaoliwang2020 <414217795zoe@gmail.com>
2021-05-26 15:42:25 +1000

    add wallets page to show all the transaction history

--- app/src/main/java/com/example/dgtcards/HomeActivity.kt
+++ app/src/main/java/com/example/dgtcards/HomeActivity.kt
@@ -2,12 +2,10 @@ package com.example.dgtcards
 
 import android.content.Intent
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -31,31 +29,38 @@ import kotlin.collections.ArrayList
 
 class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransactionItem,CardAdapter.ClickCardItem  {
 
+    // Initialize Firebase DatabaseReference and auth
     private lateinit var dbRefCards : DatabaseReference
     private lateinit var dbRefUser : DatabaseReference
     private lateinit var dbRefPayment : DatabaseReference
     private lateinit var auth: FirebaseAuth
 
+    // Initialize card list view and array and adpter to show card list
     private lateinit var cardListRecyclerView : RecyclerView
     private lateinit var cardsArrayList : ArrayList<CardModel>
     var cardAdapter : CardAdapter ? = null
 
+    // Initialize
     var residence: Boolean? = null
     var gender :String? = null
     var fullName :String ? = null
     var email:String ? = null
     var phone :String ? = null
 
+    // Initialize transaction list view and array and adpter to show transaction list
     private lateinit var transactionsRecyclerView : RecyclerView
     private var transactionItemModelList = ArrayList<TransactionItemModel>()
     var transactionItemAdapter : TransactionItemAdapter ? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        // get user's data
         getUserData()
-        // card list
+
+        // show card list
         cardListRecyclerView = findViewById(R.id.cards)
         cardListRecyclerView.layoutManager = LinearLayoutManager(this)
         cardListRecyclerView.setHasFixedSize(true)
@@ -63,7 +68,7 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
         cardsArrayList = arrayListOf<CardModel>()
         getCardData()
 
-        // transaction list
+        // show transaction list
         transactionsRecyclerView = findViewById(R.id.transaction_history_list)
         transactionsRecyclerView.layoutManager = LinearLayoutManager(this)
         transactionsRecyclerView.setHasFixedSize(true)
@@ -72,18 +77,18 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
         getPaymentsData()
     }
 
-
+    // click transaction list item to check transaction details
     override fun clickTransactionItem(itemModel: TransactionItemModel) {
-
         var transactionItem = itemModel
         startActivity(Intent(this@HomeActivity,TransactionDetailsActivity::class.java).putExtra("data",transactionItem))
     }
-
+    // click card list item to check card details
     override fun ClickCardItem(itemModel: CardModel) {
         var card = itemModel
         startActivity(Intent(this@HomeActivity,CardDetailsActivity::class.java).putExtra("data",card))
     }
 
+    // get user data
     private fun getUserData(){
         // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
@@ -95,7 +100,6 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
             override fun onCancelled(error: DatabaseError) {
                 Toast.makeText(this@HomeActivity,error.toString(),Toast.LENGTH_LONG).show()
             }
-
             override fun onDataChange(snapshot: DataSnapshot) {
                 userName.text = "Welcome " + snapshot.child("fullName").value.toString()
                 fullName = snapshot.child("fullName").value.toString()
@@ -105,8 +109,8 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
         })
     }
 
+    // get card list data by user id
     private fun getCardData(){
-
         dbRefCards = FirebaseDatabase.getInstance().getReference("Cards")
         val user = auth.currentUser
         val userCards: Query = dbRefCards.orderByChild("userId").equalTo(user!!.uid)
@@ -131,18 +135,21 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
                     empty_view.isVisible = false
                 }
                 else{
+                    // if no data show notice
                     empty_view.isVisible = true
                 }
             }
-
         })
     }
 
+    // get payments data by user id
     private fun getPaymentsData(){
 
         dbRefPayment = FirebaseDatabase.getInstance().getReference("Payments")
         val user = auth.currentUser
-        val userPayments: Query = dbRefPayment.orderByChild("userId").equalTo(user!!.uid)
+
+        // query payments data by user id and limit to last 5
+        val userPayments: Query = dbRefPayment.orderByChild("userId").equalTo(user!!.uid).limitToLast(5);
         userPayments.addValueEventListener(object : ValueEventListener{
             override fun onCancelled(error: DatabaseError) {
                 Toast.makeText(this@HomeActivity,error.toString(),Toast.LENGTH_LONG).show()
@@ -160,18 +167,18 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
                     transactionItemAdapter!!.setData(transactionItemModelList)
 
                     transactionsRecyclerView.adapter = transactionItemAdapter
+                    empty_transactions.isVisible = false
                 }
                 else{
-                    // empty_view.isVisible = true
+                    // if no data of payments history
+                    empty_transactions.isVisible = true
                 }
             }
 
         })
     }
-
-    @RequiresApi(Build.VERSION_CODES.O)
+    // create a new transportation card
     fun createCard(view: View) {
-
         val dialog = BottomSheetDialog(this)
         val offsetFromTop = 150
         dialog.behavior.apply {
@@ -241,7 +248,7 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
         }
     }
 
-
+    // when the residence value change
     fun onRadioResidenceClicked(view: View){
         if (view is RadioButton) {
             // Is the button now checked?
@@ -252,7 +259,6 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
                 R.id.isResidence ->
                     if (checked) {
                         residence = true
-
                     }
                 R.id.notResidence ->
                     if (checked) {
@@ -261,6 +267,7 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
             }
         }
     }
+    // when gender's value change
     fun onRadioGenderClicked(view: View){
         if (view is RadioButton) {
             // Is the button now checked?
@@ -272,24 +279,22 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
                 R.id.genderFemale ->
                     if (checked) {
                         gender = view.findViewById<RadioButton>(R.id.genderFemale).text.toString()
-                        //Toast.makeText(this,gender,Toast.LENGTH_LONG).show()
                     }
                 R.id.genderMale ->
                     if (checked) {
                         gender = view.findViewById<RadioButton>(R.id.genderMale).text.toString()
-                        //Toast.makeText(this,gender,Toast.LENGTH_LONG).show()
                     }
             }
         }
     }
 
+    // create a new card form Validate
     private fun formValidate(view: View):Boolean{
 
         val city = view.findViewById<EditText>(R.id.city)
         val cardHolderName = view.findViewById<EditText>(R.id.cardHolderNameInput)
         val currentAddress = view.findViewById<EditText>(R.id.CurrentAddressInput)
         val email = view.findViewById<EditText>(R.id.EmailInput)
-
 
         if(city.text.toString().isEmpty()){
             city.error ="city should not be blank"
@@ -307,33 +312,46 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
         return true
     }
 
+    // show the bottom sheet of  user profile
     fun showUserProfile(view: View){
+
+        // create a bottom sheet dialog
         val profileDialog = BottomSheetDialog(this)
 
+        // set the view of this dialog
         val view = layoutInflater.inflate(R.layout.user_profile_layout,null)
 
+        // get the close button
         val close = view.findViewById<ImageView>(R.id.profile_close)
 
+        // when click close button close the dialog
         close.setOnClickListener{
             profileDialog.dismiss()
         }
 
+        // set the default value of each EditText
         view.findViewById<EditText>(R.id.profile_fullName).setText(fullName)
         view.findViewById<EditText>(R.id.profile_email).setText(email)
         view.findViewById<EditText>(R.id.profile_phone).setText(phone)
 
+        // set the content view of this dialog
         profileDialog.setContentView(view)
+        // show the dialog
         profileDialog.show()
 
+        // get the update button
         val update = view.findViewById<Button>(R.id.Update)
 
+        // when click update button, update user information and close the dialog
         update.setOnClickListener{
             changeUserInfo(view)
             profileDialog.dismiss()
         }
 
+        // get the change password button
         val changePwd = view.findViewById<TextView>(R.id.changePassword)
 
+        // when click change password, close profile dialog and show change password dialog.
         changePwd.setOnClickListener{
             profileDialog.dismiss()
             showChangePwdDialog()
@@ -348,7 +366,7 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
                 null,
                 false
             )
-
+        // create a alert dialig
         val customDialog = AlertDialog.Builder(this, 0).create()
 
         customDialog.apply {
@@ -356,9 +374,11 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
             setCancelable(false)
         }.show()
 
+        // binding close button
         dialogBinding?.noticeClose?.setOnClickListener{
             customDialog.dismiss()
         }
+        // binding submit button and get the passwords
         dialogBinding?.btnOk?.setOnClickListener {
             val currentPwd = dialogBinding.currentPasswordInput.text.toString()
             val newPwd = dialogBinding.newPasswordInput.text.toString()
@@ -370,6 +390,7 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
             }
         }
     }
+    // update user information to firebase by user id
     private fun changeUserInfo(view:View){
         dbRefUser = FirebaseDatabase.getInstance().getReference("Users")
         val user = auth.currentUser
@@ -378,6 +399,7 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
         val newPhoneNumber = view.findViewById<EditText>(R.id.profile_phone).text.toString()
 
         if(user != null && user.email != null){
+            // update full name
             dbRefUser.child(user.uid).child("fullName").setValue(newFullName).addOnCompleteListener{
                 if(it.isSuccessful){
                     Toast.makeText(this,"full Name change successfully",Toast.LENGTH_LONG).show()
@@ -385,6 +407,7 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
                     Toast.makeText(this,"full Name change failed",Toast.LENGTH_LONG).show()
                 }
             }
+            // update phone
             dbRefUser.child(user.uid).child("phone").setValue(newPhoneNumber).addOnCompleteListener{
                 if(it.isSuccessful){
                     Toast.makeText(this,"Phone number change successfully",Toast.LENGTH_LONG).show()
@@ -397,18 +420,24 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
             finish()
         }
     }
+    // change password by using firebase
     private fun changePassword(currentPwd:String,newPwd:String){
         dbRefUser = FirebaseDatabase.getInstance().getReference("Users")
         val user = auth.currentUser
         if(user != null && user.email != null){
             val credential = EmailAuthProvider.getCredential(user.email!!,currentPwd)
+            // re authenticate
             user.reauthenticate(credential).addOnCompleteListener{
                 if(it.isSuccessful){
+                    // if re authenticate success
                     Toast.makeText(this,"Re-authenticated success",Toast.LENGTH_LONG).show()
+                    // apply to update password
                     user.updatePassword(newPwd).addOnCompleteListener{ task->
                         if(task.isSuccessful){
+                            // update new password to database as well.
                             dbRefUser.child(user.uid).child("password").setValue(newPwd)
                             Toast.makeText(this,"Password changed successfully",Toast.LENGTH_LONG).show()
+                            // sign out
                             auth.signOut()
                             startActivity(Intent(this,MainActivity::class.java))
                             finish()
@@ -423,11 +452,15 @@ class HomeActivity : AppCompatActivity(),TransactionItemAdapter.ClickTransaction
             finish()
         }
     }
-
+    // sign out
     fun logout(view:View){
         auth.signOut()
         startActivity(Intent(this@HomeActivity, MainActivity::class.java))
         finish()
+    }
+    // navigate to wallets page to check all transactions
+    fun checkAllTransactions(view: View) {
+        startActivity(Intent(this@HomeActivity, Wallets::class.java))
     }
 }
 
---
+++ app/src/main/java/com/example/dgtcards/Wallets.kt
@@ -0,0 +1,141 @@
+package com.example.dgtcards
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.synnapps.carouselview.CarouselView
+import com.synnapps.carouselview.ImageListener
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_home.empty_transactions
+import kotlinx.android.synthetic.main.activity_wallets.*
+
+class Wallets : AppCompatActivity(),TransactionItemAdapter.ClickTransactionItem {
+
+    private lateinit var dbRefPayment : DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
+    // initialize carousel array and view
+    var imageArray: ArrayList<Int> = ArrayList()
+    var carouselView:CarouselView? = null
+
+    // Initialize transaction list view and array and adpter to show transaction list
+    private lateinit var transactionsRecyclerView : RecyclerView
+    private var transactionItemModelList = ArrayList<TransactionItemModel>()
+    var transactionItemAdapter : TransactionItemAdapter ? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallets)
+
+        imageArray.add(R.drawable.adv1)
+        imageArray.add(R.drawable.adv2)
+        imageArray.add(R.drawable.adv3)
+        imageArray.add(R.drawable.adv4)
+
+        carouselView = findViewById(R.id.carouselView)
+        carouselView!!.pageCount = imageArray.size
+
+        carouselView!!.setImageListener(imageListener)
+
+        auth = FirebaseAuth.getInstance()
+
+        // show transaction list
+        transactionsRecyclerView = findViewById(R.id.transaction_history_list)
+        transactionsRecyclerView.layoutManager = LinearLayoutManager(this)
+        transactionsRecyclerView.setHasFixedSize(true)
+
+        transactionItemModelList = arrayListOf<TransactionItemModel>()
+        getPaymentsData()
+
+
+        //initialize array for line chart
+        val entries = ArrayList<Entry>()
+
+       //add entry data to arrays
+        entries.add(Entry(1f, 10f))
+        entries.add(Entry(2f, 2f))
+        entries.add(Entry(3f, 7f))
+        entries.add(Entry(4f, 20f))
+        entries.add(Entry(5f, 16f))
+
+
+        // set the label of line chart
+        val vl = LineDataSet(entries, "My Transaction")
+
+        // default setting of line chart
+        vl.setDrawValues(false)
+        vl.setDrawFilled(true)
+        vl.lineWidth = 3f
+        vl.fillColor = R.color.green_700
+        vl.fillAlpha = R.color.black
+
+
+        // set xAxis of line chart
+        lineChart.xAxis.labelRotationAngle = 0f
+
+        // set the data to line chart
+        lineChart.data = LineData(vl)
+    }
+
+    // image listener for carousel
+    var imageListener = ImageListener{ position, imageView ->imageView.setImageResource(imageArray[position])}
+
+    // click transaction list item to check transaction details
+    override fun clickTransactionItem(itemModel: TransactionItemModel) {
+        var transactionItem = itemModel
+        startActivity(Intent(this@Wallets,TransactionDetailsActivity::class.java).putExtra("data",transactionItem))
+    }
+
+    // get all payments data by user id
+    private fun getPaymentsData(){
+
+        dbRefPayment = FirebaseDatabase.getInstance().getReference("Payments")
+        val user = auth.currentUser
+
+        // query payments data by user id
+        val userPayments: Query = dbRefPayment.orderByChild("userId").equalTo(user!!.uid);
+        userPayments.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@Wallets,error.toString(), Toast.LENGTH_LONG).show()
+            }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for(paymentSnapshot in snapshot.children){
+                        var payment = paymentSnapshot.getValue(TransactionItemModel::class.java)
+                        if (payment != null) {
+                            transactionItemModelList.add(payment)
+                        }
+                    }
+
+                    transactionItemAdapter = TransactionItemAdapter(this@Wallets)
+                    transactionItemAdapter!!.setData(transactionItemModelList)
+
+                    transactionsRecyclerView.adapter = transactionItemAdapter
+                    empty_transactions.isVisible = false
+                }
+                else{
+                    // if no data of payments history
+                    empty_transactions.isVisible = true
+                }
+            }
+
+        })
+    }
+    // back to home page
+    fun navigateHome(view: View) {
+        val intent = Intent(this, HomeActivity::class.java).apply {}
+        startActivity(intent)
+    }
+}
+
+
